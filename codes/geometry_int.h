e2beb226c7689748fb09840fb02a1a9768697d81
Varga Peter <varga.peter2500@gmail.com>
2024-09-05 10:38:56 +0200

    geometry init

---
+++ codes/geometry_int.h
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct point{
+    long long x, y;
+    point operator+(const point& p) const { return {x + p.x, y + p.y}; }
+    point operator-(const point& p) const { return {x - p.x, y - p.y}; }
+    point operator*(long long t) const { return {x * t, y * t}; }
+    bool operator==(const point& p) const { return x == p.x && y == p.y; }
+    long long len() const { return x * x + y * y; }
+};
+
+inline long long dot(const point& a, const point& b) { return a.x * b.x + a.y * b.y; }
+inline long long cross(const point& a, const point& b) { return a.x * b.y - b.x * a.y; }
+inline int sgn(long long x) { return (x > 0) - (x < 0); }
+inline int dir(const point& a, const point& b, const point& c) { return sgn(cross(b - a, c - a)); }
+
+bool comp_args(const point& a, const point& b){
+    bool fa = a.y > 0 || (a.y == 0 && a.x >= 0); 
+    bool fb = b.y > 0 || (b.y == 0 && b.x >= 0);
+    if(fa != fb) return fa;
+    long long c = cross(a, b);
+    return c != 0 ? c > 0 : a.len() < b.len();
+}
+
+inline bool contains(const point& a, const point& b, const point& p){
+    if(dir(a, b, p) != 0) return false;
+    long long d = dot(b - a, p - a);
+    return 0 <= d && d <= (b-a).len();
+}
+
+inline bool intersect1(long long a1, long long a2, long long b1, long long b2){
+    return max(min(a1, a2), min(b1, b2)) <= min(max(a1, a2), max(b1, b2));
+}
+
+inline bool intersect(const point& a1, const point& a2, const point& b1, const point& b2){
+    if(dir(b1, a1, b2) == 0 && dir(b1, a2, b2) == 0) 
+        return intersect1(a1.x, a2.x, b1.x, b2.x) && intersect1(a1.y, a2.y, b1.y, b2.y);
+    return dir(a1, a2, b1) != dir(a1, a2, b2) && dir(b1, b2, a1) != dir(b1, b2, a2);
+}
+
+vector<point> convex_hull(vector<point> a){
+    if(a.empty()) return {};
+    int pos = min_element(a.begin(), a.end(), [](const point& a, const point& b) { return a.x < b.x || (a.x == b.x && a.y < b.y); }) - a.begin();
+    swap(a[0], a[pos]);
+    sort(a.begin() + 1, a.end(), [o = a[0]](const point& a, const point& b) { int d = dir(o, a, b); return d == 1 || (d == 0 && (a-o).len() < (b-o).len()); });
+    vector<point> hull;
+    for(const point &p : a){
+        while(hull.size() > 1 && dir(hull[hull.size() - 2], hull[hull.size() - 1], p) != 1) hull.pop_back();
+        hull.push_back(p);
+    }
+    int j = (int)hull.size() - 2;
+    while(j > 0 && dir(hull[j], hull[j+1], hull[0]) != 1) {
+        hull.pop_back();
+        j--;
+    }
+
+    if(hull.size() == 2 && hull[0] == hull[1]) hull.pop_back();
+
+    return hull;
+}
+
+vector<point> minkowski_sum(vector<point> a, vector<point> b){
+    if(a.empty() || b.empty()) return {};
+    auto comp = [](const point& a, const point& b) { return a.y < b.y || (a.y == b.y && a.x < b.x); };
+    int min_a = min_element(a.begin(), a.end(), comp) - a.begin();
+    int min_b = min_element(b.begin(), b.end(), comp) - b.begin();
+    rotate(a.begin(), a.begin() + min_a, a.end());
+    rotate(b.begin(), b.begin() + min_b, b.end());
+    a.push_back(a[0]);
+    a.push_back(a[1]);
+    b.push_back(b[0]);
+    b.push_back(b[1]);
+    vector<point> hull;
+    int i = 0, j = 0;
+    while(i < a.size() - 2 || j < b.size() - 2) {
+        hull.push_back(a[i] + b[j]);
+        long long c = cross(a[i + 1] - a[i], b[j + 1] - b[j]);
+        if(c >= 0 && i < a.size() - 2)
+            ++i;
+        if(c <= 0 && j < b.size() - 2)
+            ++j;
+    }
+    return hull;
+}
