de533f3e9c182d44965f44267609aa1774649eb0
Varga Peter <varga.peter2500@gmail.com>
2024-09-11 19:09:37 +0200

    centroid,heavylight,str algs

---
+++ codes/centroid.h
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+    vector<int> to;
+    vector<pair<int, int>> p; // csak ha kell
+    int sz = 0;
+    bool vis = false;
+}; 
+vector<node> g;
+int get_sz(int x, int p = -1){
+    g[x].sz = 1;
+    for(int y : g[x].to) if(y != p && !g[y].vis) g[x].sz += get_sz(y, x);
+    return g[x].sz;
+}
+pair<int, int> get_c(int x, int n, int p = -1) { // előtte: get_sz
+    for(int y : g[x].to) if(y != p && !g[y].vis && g[y].sz * 2 >= n) return g[y].sz * 2 == n ? make_pair(x, y) : get_c(y, n, x);
+    return make_pair(x, x);
+}
+void dfs_sub(int x, int c, int d = 0, int p = -1){
+    g[x].p.emplace_back(c, d);
+    for(int y : g[x].to) if(y != p && !g[y].vis) dfs_sub(y, c, d + 1, x);
+}
+void centroid_decomp(int c){
+    int sz = get_sz(c);
+    c = get_c(c, sz).first;
+    g[c].vis = true;
+    dfs_sub(c, c); // centroid szülők távolsága a csúcstól (önmagát is beleértve) | a sz itt már nem jó újra kell számolni
+    // calc
+    for(int y : g[c].to) if(!g[y].vis) centroid_decomp(y);
+}
---
+++ codes/heavylight.h
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    vector<int> to;
+    int l, r, i, p, hld_p, sz, d; // l: st bal, r: st jobb, i: st idx, p: ős, hld_p: light edge előtti ős, sz: részfa mérete, d: gyökértől vett távolság
+    // heavy út: [l, r], részfa: [i, i + sz), !!! szegmensfában g[x].i-t kell használni
+};
+vector<node> g;
+int dfs_sz(int x, int d = 0, int p = -1){
+    g[x].sz = 1;
+    g[x].d = d;
+    for(int y : g[x].to) if(y != p) g[x].sz += dfs_sz(y, d + 1, x);
+    return g[x].sz;
+}
+int IDX = 0; // reset
+int dfs_hld(int x, int hld_p, int p = -1){ // x = hld_p = root
+    g[x].i = g[x].r = IDX++;
+    g[x].l = g[hld_p].i;
+    g[x].p = p;
+    g[x].hld_p = hld_p;
+    sort(g[x].to.begin(), g[x].to.end(), [](int i, int j){ return g[i].sz > g[j].sz; });
+    bool fst = true;
+    for(int y : g[x].to){
+        if(y == p) continue;
+        if(fst) { g[x].r = dfs_hld(y, hld_p, x); fst = false; }
+        else dfs_hld(y, y, x);
+    }
+    return g[x].r;
+}
+void build_hld(int root) { dfs_sz(root); IDX = 0; dfs_hld(root, root); }
